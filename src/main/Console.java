67e2877bb1f30a79c2ddd7237a4a9cccd9024d7b
Cat Snacks <thatcatsnacks@gmail.com>
2015-02-06 12:12:10 -0600

    Starting console implementation

---
+++ src/main/Console.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Command line implementation of the SWTORMetrics library
+ * Driven by CatConsole, the simplest console library ever
+ * written.
+ * 
+ *
+ */
+public class Console {
+	static CatConsole c;
+	
+	public static void main(String args[]){
+		console = System.console();
+	}
+}
